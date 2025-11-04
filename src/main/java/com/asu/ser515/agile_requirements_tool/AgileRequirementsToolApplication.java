2552b1a07e993d8ab87b7cf67a3e2c552af46688
Prathik <prathik612@gmail.com>
2025-11-03 22:57:12 -0700

    Initialize project structure

---
+++ src/main/java/com/asu/ser515/agile_requirements_tool/AgileRequirementsToolApplication.java
@@ -0,0 +1,13 @@
+package com.asu.ser515.agile_requirements_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileRequirementsToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileRequirementsToolApplication.class, args);
+	}
+
+}
